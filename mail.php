8f7ca9cb5b4c8bd080d6870c0bb4954c404dd577
jtaso <tsjamie1@umbc.edu>
2016-05-17 15:29:02 -0400

    updating portfolio

--- mail.php
+++ mail.php
@@ -8,10 +8,10 @@ $message        = wordwrap($_POST['message'], 70);
 if (isset( $sender, $sender_email, $message)) {
     $result = @mail( $my_email, $sender, $message, $sender_email);
 
-    print 'Your message was sent sucessfully. Thank You.';
+    echo "Your message was sent sucessfully. Thank You.";
     sleep(1);
 
-    echo "<br><a href='http://tsaojamie.tk'><button type='button'>Go Back</button></a>";
+    echo "<br><a href='http://jamietsao.tk'><button type='button'>Go Back</button></a>";
     
 } else {
   print 'Please fill out all the required information. Thank you!';
